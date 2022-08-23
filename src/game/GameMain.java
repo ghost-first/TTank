50469b6e850609e91602e89b3eb1c81bd71b5760
need_just_word <1570097048@qq.com>
2022-08-23 23:32:42 +0800

    modify

--- src/game/GameMain.java
+++ src/game/GameMain.java
@@ -1,6 +1,7 @@
 package game;
 
-//程序的入口 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
+//程序的入口 ghp_7i9mKkFgUMN93i
+// BqF5clXgFaL337I546ER2u
 public class GameMain {
     public static void main(String[] args) {
         new GameFrame();
